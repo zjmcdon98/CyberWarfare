36dec58a6ec40bd3b2bd80cf9fc5b130654d3173
zjmcdon98 <zjmcdon98@gmail.com>
2018-11-30 11:13:34 -0500

    Edit

---
+++ CyberWarfare_MVC/Controllers/AttackController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CyberWarfare_MVC.Controllers
+{
+    public class AttackController : Controller
+    {
+        // GET: Attack
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
