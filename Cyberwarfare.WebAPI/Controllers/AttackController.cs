440cf756f083e2fc3c1845504d17b35ed80e0039
zjmcdon98 <zjmcdon98@gmail.com>
2018-12-19 11:10:54 -0500

    edit

--- Cyberwarfare.WebAPI/Controllers/AttackController.cs
+++ Cyberwarfare.WebAPI/Controllers/AttackController.cs
@@ -1,4 +1,5 @@
-﻿using ClassWarfare.Services;
+﻿using ClassWarfare.Models;
+using ClassWarfare.Services;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -12,12 +13,56 @@ namespace Cyberwarfare.WebAPI.Controllers
     [Authorize]
     public class AttackController : ApiController
     {
-        public IHttpActionResult Get()
+        public IHttpActionResult GetAll()
         {
             AttackService attackService = CreateAttackService();
             var attacks = attackService.GetAttacks();
             return Ok(attacks);
         }
+
+        public IHttpActionResult Get(int id)
+        {
+            AttackService attackService = CreateAttackService();
+            var attack = attackService.GetAttackById(id);
+            return Ok(attack);
+        }
+
+        public IHttpActionResult Post(AttackCreate attack)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateAttackService();
+
+            if (!service.CreateAttack(attack))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(AttackEdit attack)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+​
+            var service = CreateAttackService();
+​
+            if (!service.UpdateAttack(attack))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateAttackService();
+​
+            if (!service.DeleteAttack(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
         private AttackService CreateAttackService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
